76ae3b3dc5a88267ee3c989aa6f27400e28a8e75
Akarsh Kumar <akarshkumar0101@gmail.com>
2022-06-13 01:31:26 -0500

    sync

--- image_cppn.py
+++ image_cppn.py
@@ -22,16 +22,20 @@ def composite_activation(x):
     # def forward():
 
 class ImageCPPN(nn.Module):
-    def __init__(self, n_hidden=24, n_layers=8, n_channels=3, activation=composite_activation):
+    def __init__(self, n_hidden=24, n_layers=8, n_channels=3, activation=composite_activation, give_radius=False):
         super().__init__()
         
+        self.give_radius = give_radius
+        
         n_features = 2
+        if self.give_radius:
+            n_features += 1
         
         n_units = [n_features] + [n_hidden]*n_layers + [n_channels]
         n_ins = np.array(n_units[:-1])
         n_outs = np.array(n_units[1:])
         
-        n_ins[1:] *= 2
+        # n_ins[1:] *= 2
         # print(n_ins, n_outs)
         self.layers = nn.ModuleList([nn.Conv2d(n_in, n_out, kernel_size=1) 
                                      for n_in, n_out in zip(n_ins, n_outs)])
@@ -54,7 +58,8 @@ class ImageCPPN(nn.Module):
         y, x = torch.linspace(-a, a, h), torch.linspace(-a, a, w)
         y, x = torch.meshgrid(y, x, indexing='ij')
         
-        # r = (x.pow(2)+y.pow(2)).sqrt()
+        r = (x.pow(2)+y.pow(2)).sqrt()
+        r = (r-r.mean())/r.std()
         # theta = torch.atan2(x, y)
         
         # ay, ax = y.abs(), x.abs()
@@ -63,7 +68,10 @@ class ImageCPPN(nn.Module):
         
         
         # x = tf.expand_dims(tf.stack([x, y], -1), 0)  # add batch dimension
-        x = torch.stack([x, y], dim=0)[None]
+        data = [x, y]
+        if self.give_radius:
+            data.append(r)
+        x = torch.stack(data, dim=0)[None]
         
         # plt.imshow(x[0, 1])
         # plt.colorbar()
--- optimization.py
+++ optimization.py
@@ -1,4 +1,80 @@
 
+import matplotlib.pyplot as plt
+from tqdm import tqdm
 
+import numpy as np
+import torch
+from torchvision import transforms
+import clip
+
+augment_trans = transforms.Compose([
+    transforms.RandomPerspective(distortion_scale=0.5, p=1, fill=1),
+    transforms.RandomResizedCrop(224, scale=(0.7,0.9)),
+])
+
+mean = [0.48145466, 0.4578275, 0.40821073]
+std = [0.26862954, 0.26130258, 0.27577711]
+
+def augment_img(img, n_augments, augment='noise'):
+    imgs = [img]
+    
+    if augment=='noise':
+        for i in range(n_augments):
+            imgs.append(img+1e-2*torch.randn_like(img))
+    if augment=='crops':
+        for i in range(n_augments):
+            imgs.append(augment_trans(img))
+    
+    return torch.stack(imgs, dim=0)
+
+def imshow(img, mean=mean, std=std):
+    """
+    img.shape should be (3, h, w) on any device, torch tensor
+    """
+    img = img.detach().cpu()
+    mean = torch.tensor(mean)
+    std = torch.tensor(std)
+    img = img.permute(1, 2, 0)
+    img = img*std+mean
+    img = img.clamp(0, 1).numpy()
+    plt.imshow(img)
+
+def optimize_imgs(model, texts, n_iterations=100, imgs=None, img_size=None, device='cpu', show_every_k=None):
+
+    model = model.to(device)
+
+    with torch.no_grad():
+        texts_tokens = clip.tokenize(texts).to(device)
+        texts_features = model.encode_text(texts_tokens)
+
+    target = texts_features.detach()
+
+    if imgs is None:
+        imgs = 1e-2*torch.randn(len(target), 3, 224, 224)
+    imgs = imgs.clone().to(device).requires_grad_()
+
+    opt = torch.optim.Adam([imgs], lr=3e-2, weight_decay=1e-6)
+
+    for i in tqdm(range(n_iterations)):
+        imgs_augments = augment_img(imgs, 3, augment='crops')
+        shape_aug = imgs_augments.shape
+        imgs_features = model.encode_image(imgs_augments.view(-1, *shape_aug[-3:]))
+        imgs_features = imgs_features.view(*shape_aug[:2], -1)
+        
+        dots = torch.cosine_similarity(imgs_features, target, dim=-1)
+        loss = -dots.mean()
+        
+        opt.zero_grad()
+        loss.backward()
+        opt.step()
+
+        if show_every_k is not None and i%show_every_k==0:
+            dots_img = dots.mean(dim=0).tolist()
+            plt.figure(figsize=(5*len(imgs), 5))
+            for i_img, img in enumerate(imgs):
+                plt.subplot(1, len(imgs), i_img+1)
+                plt.title(texts[i_img] + f', match: {dots_img[i_img]:.4f}')
+                imshow(img)
+            plt.show()
 
 
