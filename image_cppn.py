72aaa6bd872794ed741fee83ecb6545d77232a72
Akarsh Kumar <akarshkumar0101@gmail.com>
2022-06-13 18:22:47 -0500

    organize BatchImageCPPN and ImageDOFs into classes

--- image_cppn.py
+++ image_cppn.py
@@ -1,4 +1,5 @@
 
+from distutils.ccompiler import gen_lib_options
 import numpy as np
 import torch
 from torch import nn
@@ -22,20 +23,21 @@ def composite_activation(x):
     # def forward():
 
 class ImageCPPN(nn.Module):
-    def __init__(self, n_hidden=24, n_layers=8, n_channels=3, activation=composite_activation, give_radius=False):
+    def __init__(self, n_hidden=24, n_layers=8, n_channels=3, activation=torch.relu, features=['x', 'y', 'r'], cache_coordinates=True):
         super().__init__()
         
-        self.give_radius = give_radius
+        self.features = features
+        self.idx_features_to_use = []
+        feature2idx = {'y': 0, 'x': 1, 'r': 2, 'theta': 3, 'ay': 4, 'ax': 5}
+        for feature in self.features:
+            self.idx_features_to_use.append(feature2idx[feature])
+
+        self.cache_coordinates = {} if cache_coordinates else None
         
-        n_features = 2
-        if self.give_radius:
-            n_features += 1
-        
-        n_units = [n_features] + [n_hidden]*n_layers + [n_channels]
+        n_units = [len(self.idx_features_to_use)] + [n_hidden]*n_layers + [n_channels]
         n_ins = np.array(n_units[:-1])
         n_outs = np.array(n_units[1:])
         
-        # n_ins[1:] *= 2
         # print(n_ins, n_outs)
         self.layers = nn.ModuleList([nn.Conv2d(n_in, n_out, kernel_size=1) 
                                      for n_in, n_out in zip(n_ins, n_outs)])
@@ -44,36 +46,54 @@ class ImageCPPN(nn.Module):
         self.activation = activation
         
     def forward(self, x):
+        x = x[:, self.idx_features_to_use, :, :]
+
         for layer in self.layers[:-1]:
             x = layer(x)
             x = self.activation(x)
         x = self.layers[-1](x)
         x = x.sigmoid()
         return x
-    
-    def generate_image(self, img_size):
-        a = 3.0**0.5  # std(coord_range) == 1.0
+
+    def generate_input(self, img_size=(224, 224)):
+        device = self.layers[0].weight.device
+
+        if self.cache_coordinates is not None and img_size in self.cache_coordinates:
+            return self.cache_coordinates[img_size].to(device)
+
         h, w = img_size
-        
-        y, x = torch.linspace(-a, a, h), torch.linspace(-a, a, w)
+        y, x = torch.linspace(0, 1, h, device=device), torch.linspace(0, 1, w, device=device)
         y, x = torch.meshgrid(y, x, indexing='ij')
         
         r = (x.pow(2)+y.pow(2)).sqrt()
-        r = (r-r.mean())/r.std()
-        # theta = torch.atan2(x, y)
+        theta = torch.atan2(x, y)
+        ay, ax = y.abs(), x.abs()
         
-        # ay, ax = y.abs(), x.abs()
-        
-        # plt.colorbar()
-        
-        
-        # x = tf.expand_dims(tf.stack([x, y], -1), 0)  # add batch dimension
-        data = [x, y]
-        if self.give_radius:
-            data.append(r)
-        x = torch.stack(data, dim=0)[None]
-        
-        # plt.imshow(x[0, 1])
-        # plt.colorbar()
-        x = x.to(self.layers[0].weight.device)
+        data = [y, x, r, theta, ay, ax]
+        x = torch.stack(data, dim=0)[None] # 1, 6, h, w
+        # normalize
+        x = (x-x.mean(dim=(-1, -2), keepdim=True))/x.std(dim=(-1, -2), keepdim=True)
+
+        if self.cache_coordinates is not None:
+            self.cache_coordinates[img_size] = x
+
+        return x
+
+    def generate_image(self, img_size=(224, 224)):
+        x = self.generate_input(img_size)
         return self(x)
+
+
+class BatchImageCPPN(nn.Module):
+    def __init__(self, n_batch=1, n_hidden=24, n_layers=8, n_channels=3, activation=torch.relu, give_radius=False, cache_coordinates=True):
+        super().__init__()
+        self.cppns = nn.ModuleList([ImageCPPN(n_hidden, n_layers, n_channels, activation, give_radius, cache_coordinates) for _ in range(n_batch)])
+
+    def forward(self, x):
+        return torch.cat([cppn(x) for cppn in self.cppns], dim=0)
+
+    def generate_image(self, img_size=(224, 224)):
+        x = self.cppns[0].generate_input(img_size)
+        return self(x)
+
+        
--- optimization.py
+++ optimization.py
@@ -7,6 +7,10 @@ import torch
 from torchvision import transforms
 import clip
 
+from torch import nn
+
+from image_cppn import ImageCPPN, BatchImageCPPN
+
 augment_trans = transforms.Compose([
     transforms.RandomPerspective(distortion_scale=0.5, p=1, fill=1),
     transforms.RandomResizedCrop(224, scale=(0.7,0.9)),
@@ -15,66 +19,86 @@ augment_trans = transforms.Compose([
 mean = [0.48145466, 0.4578275, 0.40821073]
 std = [0.26862954, 0.26130258, 0.27577711]
 
-def augment_img(img, n_augments, augment='noise'):
-    imgs = [img]
+def augment_img(img, n_augments, augment='crops', keep_original=False):
+    imgs = []
+
+    if keep_original:
+        imgs.append(img)
     
     if augment=='noise':
-        for i in range(n_augments):
+        for _ in range(n_augments):
             imgs.append(img+1e-2*torch.randn_like(img))
     if augment=='crops':
-        for i in range(n_augments):
+        for _ in range(n_augments):
             imgs.append(augment_trans(img))
     
     return torch.stack(imgs, dim=0)
 
-def imshow(img, mean=mean, std=std):
-    """
-    img.shape should be (3, h, w) on any device, torch tensor
-    """
-    img = img.detach().cpu()
-    mean = torch.tensor(mean)
-    std = torch.tensor(std)
-    img = img.permute(1, 2, 0)
-    img = img*std+mean
-    img = img.clamp(0, 1).numpy()
-    plt.imshow(img)
+class ImageDOFs(nn.Module):
+    def __init__(self, n_batch=1, n_channels=3, img_size=(224, 224)):
+        super().__init__()
+        self.dofs = nn.Parameter(1e-1*torch.randn(n_batch, n_channels, *img_size))
 
-def optimize_imgs(model, texts, n_iterations=100, imgs=None, img_size=None, device='cpu', show_every_k=None):
+    def forward(self):
+        return self.dofs.sigmoid()
 
-    model = model.to(device)
+    def generate_image(self, img_size=None):
+        return self()
+
+def optimize_imgs(clip_model, texts, imgs='pixel', n_iterations=100, img_size=(224, 224), lr=3e-2, device=None, callback=None):
+    """
+    clip_model should be should be the model returned by ```model, preprocess = clip.load("ViT-B/32", device=device)```
+
+    texts should be List[str]
+    imgs should be one of
+        - 'pixel'
+        - 'cppn'
+        - torch.Tensor of shape (len(texts), 3, 224, 224)
+        - BatchImageCPPN
+
+    n_iterations should be int
+
+    img_size should be (h, w)
+
+    callback is called every iteration like 
+    ```callback(i_iteration=i_iteration, texts=texts, imgs=imgs, imgs_augments=imgs_augments, imgs_features=imgs_features, texts_features=texts_features, dots=dots, loss=loss)```
+    
+    """
+
+    clip_model = clip_model.to(device)
 
     with torch.no_grad():
         texts_tokens = clip.tokenize(texts).to(device)
-        texts_features = model.encode_text(texts_tokens)
+        texts_features = clip_model.encode_text(texts_tokens).detach() # (batch, embed_dim)
 
-    target = texts_features.detach()
+    if imgs is None or imgs=='pixel':
+        dofs = ImageDOFs(len(texts), 3, img_size).to(device)
+    elif imgs=='cppn':
+        dofs = BatchImageCPPN(len(texts), n_hidden=24, n_layers=8, n_channels=3, activation=torch.relu, cache_coordinates=True).to(device)
+    elif isinstance(imgs, torch.Tensor):
+        raise NotImplementedError
+    elif isinstance(imgs, BatchImageCPPN):
+        dofs = imgs.to(device)
 
-    if imgs is None:
-        imgs = 1e-2*torch.randn(len(target), 3, 224, 224)
-    imgs = imgs.clone().to(device).requires_grad_()
+    opt = torch.optim.Adam(dofs.parameters(), lr=lr, weight_decay=1e-6)
 
-    opt = torch.optim.Adam([imgs], lr=3e-2, weight_decay=1e-6)
+    for i_iteration in tqdm(range(n_iterations)):
+        imgs = dofs.generate_image()
 
-    for i in tqdm(range(n_iterations)):
-        imgs_augments = augment_img(imgs, 3, augment='crops')
+        imgs_augments = augment_img(imgs, 3, augment='crops') # (n_augs, batch, 3, h, w)
         shape_aug = imgs_augments.shape
-        imgs_features = model.encode_image(imgs_augments.view(-1, *shape_aug[-3:]))
-        imgs_features = imgs_features.view(*shape_aug[:2], -1)
+        clip_input = imgs_augments.view(-1, *shape_aug[-3:]) *2.-1. # go from [0, 1] to [-1, 1]
+        imgs_features = clip_model.encode_image(clip_input)
+        imgs_features = imgs_features.view(*shape_aug[:2], -1) # (n_augs, batch, embed_dim)
         
-        dots = torch.cosine_similarity(imgs_features, target, dim=-1)
+        dots = torch.cosine_similarity(imgs_features, texts_features, dim=-1)
         loss = -dots.mean()
         
         opt.zero_grad()
         loss.backward()
         opt.step()
 
-        if show_every_k is not None and i%show_every_k==0:
-            dots_img = dots.mean(dim=0).tolist()
-            plt.figure(figsize=(5*len(imgs), 5))
-            for i_img, img in enumerate(imgs):
-                plt.subplot(1, len(imgs), i_img+1)
-                plt.title(texts[i_img] + f', match: {dots_img[i_img]:.4f}')
-                imshow(img)
-            plt.show()
-
+        if callback is not None:
+            callback(i_iteration=i_iteration, texts=texts, imgs=imgs, imgs_augments=imgs_augments, 
+                     imgs_features=imgs_features, texts_features=texts_features, dots=dots, loss=loss)
 
